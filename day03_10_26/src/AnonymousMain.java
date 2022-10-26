7775c5ad4cf448ce353841ad30b024d17cfdaf3d
user <user@DESKTOP-81MKMOM>
2022-10-26 12:26:11 +0900

    1026

--- day03_10_26/src/AnonymousMain.java
+++ day03_10_26/src/AnonymousMain.java
@@ -4,6 +4,14 @@ interface Starcraft{
 	
 }
 
+
+//인터페이스를 구현한 클래스를 생성
+class Protoss implements Starcraft{
+	public void attack() {
+		System.out.println("질럿의 공격");
+	}
+}
+
 public class AnonymousMain {
 	public static void main(String[] args) {
 		
